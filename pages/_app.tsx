c67858b8640cc459a3e63ef1965a47e1c8fc20fa
Ozer <ozer.gul@cdev.global>
2021-02-07 13:34:22 +0300

    firebase removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,24 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 
 import { AppProps } from "next/app";
-import "firebase/auth";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { wrapper } from "../store/store";
+import React from "react";
 
 library.add(fas);
 
-/**
- * <FirebaseAuthProvider firebase={firebase} {...config}>
-
- </FirebaseAuthProvider>
- * @param Component
- * @param pageProps
- * @constructor
- */
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default wrapper.withRedux(App);
+export default App;
